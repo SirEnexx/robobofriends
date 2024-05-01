ecf8da96837377b91401e0884440d9402dace81c
Farhan Nadim <farhan7733550@gmail.com>
2024-05-01 00:10:35 -0400

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ import {robobots} from './robobots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Card />, document.getElementById('root')
+  <div>
+    <Card id={robobots[0].id} name={robobots[0].name} email={robobots[0].email}/>
+    <Card id={robobots[1].id} name={robobots[1].name} email={robobots[1].email}/>
+    <Card id={robobots[2].id} name={robobots[2].name} email={robobots[2].email}/>
+  </div>
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/robobots.js
@@ -0,0 +1,50 @@
+export const robobots = [
+		{
+			id: 1,
+			name: 'Pee Poo',
+			username: 'TheHalfOne',
+			email:'pee.poo@poop.com'
+		},
+		{
+			id: 2,
+			name: 'Pee Pee',
+			username: 'PEEPEE',
+			email:'pee.pee@poop.com'
+		},
+		{
+			id: 3,
+			name: 'Poo Poo',
+			username: 'POOPOO',
+			email:'poo.poo@poop.com'
+		},
+		{
+			id: 4,
+			name: 'Poo Pee',
+			username: 'TheFakeHalfOne',
+			email:'poo.pee@poop.com'
+		},
+		{
+			id: 5,
+			name: 'Satoru Gojo',
+			username: 'HonouredOne',
+			email:'thestrongest@jjk.com'
+		},
+		{
+			id: 6,
+			name: 'Suguru Geto',
+			username: 'WorstCurseUser',
+			email:'monkeyhater123@jjk.com'
+		},
+		{
+			id: 7,
+			name: 'Ryomen Sukuna',
+			username: 'DisgracedOne',
+			email:'kingofcurses@jjk.com'
+		},
+		{
+			id: 8,
+			name: 'Aoi Todo',
+			username: 'Brother',
+			email:'takadachan.no1fan@jjk.com'
+		}
+	]
