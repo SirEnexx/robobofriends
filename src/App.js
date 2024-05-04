bbf7c7d58876a174cd8673306e75c0d727688f54
Farhan Nadim <farhan7733550@gmail.com>
2024-05-04 14:16:38 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,2 +1,33 @@
-import React from 'react';
-import CardList from 'CardList';
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robobots} from './robobots';
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robobots: robobots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		const filteredRobobots = this.state.robobots.filter(robobot => {
+			return robobots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+		console.log(event.target.value)
+	}
+
+	render() {
+		return (
+			<div className='tc'>
+				<h1>RoboboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList robobots={this.state.robobots} />
+			</div>
+		)
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robobots} from './robobots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <CardList robobots={robobots} />
-  , document.getElementById('root')
+root.render(<App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
